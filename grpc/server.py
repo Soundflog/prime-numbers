613dc87aa0a3d39efb8b58bfc5ff973665ee7c9c
stud130312 <paha-vershinin@mail.ru>
2024-10-08 18:19:41 +0300

    update RPC server & client
    добавлены остальные методы нахождения простых чисел

--- grpc/server.py
+++ grpc/server.py
@@ -13,19 +13,33 @@ with SimpleXMLRPCServer(('localhost', 8000), requestHandler=RequestHandler, allo
     server.register_introspection_functions()
 
 
-    # Функция для проверки простоты числа
-    def is_prime(number):
+    # Функция для проверки простоты числа (метод деления)
+    def is_prime_basic(number):
         return algorithms.is_prime_basic(number)
 
 
-    # Функция для нахождения всех простых чисел до указанного предела
-    def get_primes(limit):
-        return [num for num in range(2, limit + 1) if algorithms.is_prime_basic(num)]
+    # Функция для нахождения всех простых чисел до предела (метод Решета Эратосфена)
+    def get_primes_eratosthenes(limit):
+        sieve = algorithms.sieve_eratosthenes(limit)
+        return [i for i in range(2, limit + 1) if sieve[i]]
+
+
+    # Функция для нахождения всех простых чисел до предела (метод Решета Аткина)
+    def get_primes_atkin(limit):
+        sieve = algorithms.sieve_atkin(limit)
+        return [i for i in range(2, limit + 1) if sieve[i]]
+
+
+    # Функция для проверки числа на простоту (метод Миллера-Рабина)
+    def is_prime_miller_rabin(number):
+        return algorithms.miller_rabin(number)
 
 
     # Регистрируем функции, которые могут вызываться клиентом
-    server.register_function(is_prime, 'is_prime')
-    server.register_function(get_primes, 'get_primes')
+    server.register_function(is_prime_basic, 'is_prime_basic')
+    server.register_function(get_primes_eratosthenes, 'get_primes_eratosthenes')
+    server.register_function(get_primes_atkin, 'get_primes_atkin')
+    server.register_function(is_prime_miller_rabin, 'is_prime_miller_rabin')
 
     # Запуск сервера
     print("Сервер запущен на порту 8000...")
